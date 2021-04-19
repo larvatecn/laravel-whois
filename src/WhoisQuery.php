f015e9ba85ac699d8145a177e74cd86b23a9a335
XuTongle <xutongle@gmail.com>
2021-04-19 10:46:30 +0800

    域名检测

--- src/WhoisQuery.php
+++ src/WhoisQuery.php
@@ -12,6 +12,7 @@ use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Log;
 use Iodev\Whois\Factory;
+use Pdp\Rules;
 
 /**
  * Class WhoisQuery
@@ -46,12 +47,46 @@ class WhoisQuery
 //    }
 
     /**
+     * 域名后缀解析
+     * @param string $domain
+     * @return \Pdp\Domain
+     */
+    protected function Resolve(string $domain): \Pdp\Domain
+    {
+        $rules = Rules::createFromPath('../resources/public_suffix_list.dat');
+        return $rules->resolve($domain);
+    }
+
+    /**
+     * 解析域名
+     *
+     * @param string $host
+     * @return bool|\Pdp\Domain
+     */
+    public function parseDomain(string $host)
+    {
+        if (strpos($host, '://') !== false) {
+            $url = parse_url($host);
+            if (isset ($url ['host'])) {
+                $host = $url ['host'];
+            }
+        }
+        if ($host && strpos($host, '.') !== false) {
+            return static::Resolve($host);
+        }
+        return false;
+    }
+
+    /**
      * 查询原始 Whois
      * @param string $domain
      * @return false|string
      */
     public function lookupRaw(string $domain)
     {
+        if (($domain = $this->parseDomain($domain)) == false) {
+            return false;
+        }
         // Creating default configured client
         $whois = Factory::get()->createWhois();
         try {
@@ -69,6 +104,9 @@ class WhoisQuery
      */
     public function lookupInfo(string $domain)
     {
+        if (($domain = $this->parseDomain($domain)) == false) {
+            return false;
+        }
         try {
             // Creating default configured client
             $whois = Factory::get()->createWhois();
@@ -88,6 +126,9 @@ class WhoisQuery
      */
     public function lookup(string $domain, $refresh = false)
     {
+        if (($domain = $this->parseDomain($domain)) == false) {
+            return false;
+        }
         if ($refresh == false && ($info = Domain::getDomainInfo($domain)) != false) {
             return $info;
         } else {
